929dfd952c319e66adf4db96b12c7aa06508f88e
bayoh807@gmail.com <you@example.com>
2020-10-21 00:40:02 +0800

    區分生產環境和開發環境

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
     { 
-        path: '/pwa/',
-        redirect: '/pwa/herry' 
+        path: '/',
+        redirect: '/herry' 
     },
     {
         name : 'doctor',
-        path: '/pwa/:name',
+        path: '/:name',
         component: require('@/view/main/home.vue').default
     },
-    // {
-    //   path: '/about',
-    // //   component: require('@/views/about.vue').default
-    // },
+    {
+      path: '/questionList',
+      component: require('@/view/main/questionList.vue').default
+    },
     // {
     //   path: '/:catchAll(.*)',
     // //   component: require('@/views/404.vue').default
